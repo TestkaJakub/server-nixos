292720ef8166b19b6725feaac08896bf7c219e15
TestkaJakub <84832891+TestkaJakub@users.noreply.github.com>
2026-04-03 21:58:25 +0200

    upgrade 2026-04-03 21:58

--- modules/system/tailscale.nix
+++ modules/system/tailscale.nix
@@ -1,10 +1,13 @@
 { pkgs, config, ... }:
-
-# ── Tailscale ──────────────────────────────────────────────────────────────────
 {
   services.tailscale = {
     enable             = true;
     useRoutingFeatures = "both";
+  };
+
+  boot.kernel.sysctl = {
+    "net.ipv4.ip_forward"                   = 1;
+    "net.ipv6.conf.all.forwarding"          = 1;
   };
 
   networking.firewall = {
