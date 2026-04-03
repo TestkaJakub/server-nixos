362cf5213bee02f0f17983256cba1d31836a067b
TestkaJakub <84832891+TestkaJakub@users.noreply.github.com>
2026-04-03 18:37:47 +0200

    upgrade 2026-04-03 18:37

--- modules/containers/pihole.nix
+++ modules/containers/pihole.nix
@@ -68,7 +68,7 @@
 	  "--label=traefik.http.routers.pihole.entrypoints=websecure"
 	  "--label=traefik.http.routers.pihole.tls=true"
 	  "--label=traefik.http.routers.pihole.tls.certresolver=step"
-	  "--label=traefik.http.services.pihole.loadbalancer.server.port=8053"
+	  "--label=traefik.http.services.pihole.loadbalancer.server.port=80"
 	  "--label=traefik.http.middlewares.pihole-slash.redirectregex.regex=^https://pihole.home/?$"
 	  "--label=traefik.http.middlewares.pihole-slash.redirectregex.replacement=https://pihole.home/admin/"
 	  "--label=traefik.http.routers.pihole.middlewares=pihole-slash"
