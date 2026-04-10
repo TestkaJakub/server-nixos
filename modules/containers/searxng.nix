3f1227d31e7668f58025f236edf5e2ef2f17780b
TestkaJakub <84832891+TestkaJakub@users.noreply.github.com>
2026-04-10 21:41:59 +0200

    upgrade 2026-04-10 21:41

---
+++ modules/containers/searxng.nix
@@ -0,0 +1,36 @@
+{ ... }:
+
+{
+	# Setting up searxng docker directory
+	systemd.tmpfiles.rules = [
+		"d /home/jakub/docker-data/searxng-config 0775 jakub jakub -"	
+	];
+
+	# Setting up searxng docker container
+	virtualisation.oci-containers.containers.searxng = {
+		image          = "searxng/searxng";
+		autoStart      = true;	
+
+		environment.TZ = "Europe/Warsaw";
+
+		volumes = [
+			"/home/jakub/docker-data/searxng-config:/etc/searxng"
+		];
+
+		extraOptions = [
+			"--network=traefik"
+			"--label=traefik.enable=true"
+			"--label=traefik.http.routers.searxng.rule=Host(`searxng.home`)"
+			"--label=traefik.http.routers.searxng.entrypoints=websecure"
+			"--label=traefik.http.routers.searxng.tls=true"
+			"--label=traefik.http.routers.searxng.tls.certresolver=step"
+			"--label=traefik.http.services.searxng.loadbalancer.server.port=8080"	
+		];
+	};
+
+	# Ensuring searxng service will start only if traefik network is available
+	systemd.services.docker-searxng = {
+		after    = [ "docker-network-traefik.service" ];
+		requires = [ "docker-network-traefik.service" ];	
+	};
+}
